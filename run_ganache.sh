202e9eb4ceb998380b67748898f43b8ceb77cad9
Dmytro <dmitry.tkk@gmail.com>
2020-11-12 17:45:41 +0200

    SKALE-3533 Add run ganache script

---
+++ run_ganache.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# This script can be used to deploy specified version of skale-manager to the provided endpoint
+# with given private key
+#
+
+set -e
+
+: "${ETH_PRIVATE_KEY?Need to set ETH_PRIVATE_KEY}"
+: "${MANAGER_TAG?Need to set MANAGER_TAG}"
+
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export DOCKER_NETWORK_ENDPOINT=http://ganache:8545
+export NETWORK=${NETWORK:-unique}
+export GAS_PRICE=${GAS_PRICE:-10000000000}
+
+source $DIR/helper.sh
+
+create_test_docker_network
+run_ganache $ETH_PRIVATE_KEY
