777ac45c88811685c14e6d00bd97c6f00d744ce0
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-01-22 00:21:15 +0200

    SKALE-3775 Remove comments

--- deploy_test_ima.sh
+++ deploy_test_ima.sh
@@ -15,17 +15,9 @@ export DOCKER_NETWORK_ENDPOINT=http://ganache:8545
 export NETWORK=${NETWORK:-unique}
 export GAS_PRICE=${GAS_PRICE:-10000000000}
 
-
-
 source $DIR/helper.sh
 
 create_test_docker_network
 run_ganache $ETH_PRIVATE_KEY
-#if [ ! -f "contracts_data/skaleManagerComponents.json" ]; then
-#    if [ ! -f "contracts_data/manager.json" ]; then
-#        deploy_manager $MANAGER_TAG $DOCKER_NETWORK_ENDPOINT $ETH_PRIVATE_KEY $GAS_PRICE $NETWORK
-#    fi
-#    cp contracts_data/manager.json contracts_data/skaleManagerComponents.json
-#fi
 deploy_manager $MANAGER_TAG $DOCKER_NETWORK_ENDPOINT $ETH_PRIVATE_KEY $GAS_PRICE $NETWORK
 deploy_ima_proxy $IMA_TAG $DOCKER_NETWORK_ENDPOINT $ETH_PRIVATE_KEY $GAS_PRICE
