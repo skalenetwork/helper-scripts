f04f10aad9fac92a87115d5e79bced16e9832a4d
Dmytro <dmitry.tkk@gmail.com>
2021-02-11 15:21:47 +0200

    SKALE-3877 Update helper-script for the new manager

--- deploy_test_allocator.sh
+++ deploy_test_allocator.sh
@@ -12,17 +12,17 @@ set -e
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export DOCKER_NETWORK_ENDPOINT=http://ganache:8545
 export ALLOCATOR_PRODUCTION=${ALLOCATOR_PRODUCTION:-true}
-export NETWORK=${NETWORK:-unique}
+export NETWORK=${NETWORK:-custom}
 export GAS_PRICE=${GAS_PRICE:-10000000000}
 
 source $DIR/helper.sh
 
-export NETWORK=${NETWORK:-unique}
+export ALLOCATOR_NETWORK=${ALLOCATOR_NETWORK:-unique}
 
 create_test_docker_network
 run_ganache $ETH_PRIVATE_KEY
 deploy_manager $MANAGER_TAG $DOCKER_NETWORK_ENDPOINT $ETH_PRIVATE_KEY $GAS_PRICE $NETWORK
-deploy_allocator $ALLOCATOR_TAG $DOCKER_NETWORK_ENDPOINT $ETH_PRIVATE_KEY $ALLOCATOR_PRODUCTION $GAS_PRICE $NETWORK
+deploy_allocator $ALLOCATOR_TAG $DOCKER_NETWORK_ENDPOINT $ETH_PRIVATE_KEY $ALLOCATOR_PRODUCTION $GAS_PRICE $ALLOCATOR_NETWORK
 
 MANAGER_ABI_FILE=$DIR/contracts_data/manager.json
 ALLOCATOR_ABI_FILE=$DIR/allocator_contracts_data/allocator.json
--- helper.sh
+++ helper.sh
@@ -66,7 +66,7 @@ deploy_manager () {
         -e PRIVATE_KEY=$3 \
         -e GASPRICE=$4 \
         skalenetwork/$SM_IMAGE_NAME:$1 \
-        npx truffle migrate --network $5
+        npx hardhat run migrations/deploy.ts --network $5
 
     echo Copying $DIR/contracts_data/$NETWORK.json -> $DIR/contracts_data/manager.json
     cp $DIR/contracts_data/$NETWORK.json $DIR/contracts_data/manager.json
