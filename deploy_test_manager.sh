eca9895db6e4dad1c53be0d0b345cbd1056f5baa
badrogger <ivan@skalelabs.com>
2023-08-25 12:26:53 +0000

    Add no custom network mode

--- deploy_test_manager.sh
+++ deploy_test_manager.sh
@@ -4,10 +4,12 @@
 # with given private key
 #
 
-set -e
+set -ea
 
 : "${ETH_PRIVATE_KEY?Need to set ETH_PRIVATE_KEY}"
 : "${MANAGER_TAG?Need to set MANAGER_TAG}"
+
+
 
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export DOCKER_NETWORK_ENDPOINT=http://ganache:8545
@@ -17,6 +19,18 @@ export ETHERSCAN=${ETHERSCAN:-1234}
 
 source $DIR/helper.sh
 
-create_test_docker_network
-run_ganache $ETH_PRIVATE_KEY
+if [[ $CUSTOM_NETWORK ]]; then
+    create_test_docker_network
+    export DOCKER_NETWORK_ENDPOINT=http://ganache:8545
+    export DOCKER_NETWORK=testnet
+else
+    export DOCKER_NETWORK_ENDPOINT=http://127.0.0.1:8545
+    export DOCKER_NETWORK=host
+fi
+
+
+if [[ $GANACHE ]]; then
+    run_ganache $ETH_PRIVATE_KEY
+fi
+
 deploy_manager $MANAGER_TAG $DOCKER_NETWORK_ENDPOINT $ETH_PRIVATE_KEY $GAS_PRICE $NETWORK $ETHERSCAN
