939773e85bae1b06da34beffc3d35142bbef04aa
badrogger <ivan@skalelabs.com>
2025-06-17 12:29:48 +0100

    Update redis container version

--- redis/run.sh
+++ redis/run.sh
@@ -2,4 +2,5 @@ set -e
 
 docker rm -f redis || true
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docker run -v $DIR/conf:/config --network=host --name=redis -d redis:6.0-alpine
+docker run -v $DIR/conf:/config --network=host --name=redis -d redis:8.0.1-alpine
+
